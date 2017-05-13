f6d50abce74aed6163f14818e516f6a825032ea8
Mathias Olsson <hypp74@gmail.com>
2017-05-13 11:47:10 +0200

    Initial checkin

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
+use std::io::BufReader;
+use std::io::Read;
+use std::cmp;
+use std::str::FromStr;
+use std::collections::BTreeMap;
+
+extern crate modfile;
+use modfile::ptmf;
+
+extern crate rustc_serialize;
+extern crate docopt;
+use docopt::Docopt;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+static USAGE: &'static str = "
+mod2abc.
+
+Usage: 
+    mod2abc (-h | --help)
+    mod2abc (-V | --version)
+    mod2abc --in=<filename> --out=<filename>
+
+Options:
+    -V, --version         Show version info.
+    -h, --help            Show this text.
+	--in=<filename>       Name of inputfile
+	--out=<filename>      Name of outputfile
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    flag_help: bool,
+	flag_version: bool,
+	
+	flag_in: String,
+	flag_out: String,
+}
+
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+//    println!("{:?}", args);	
+	
+	if args.flag_version {
+		println!("Version: {}", VERSION);
+		return;
+	}
+	
+	if args.flag_in == "" {
+		println!("No inputfile specificed");
+		return
+	}
+
+	if args.flag_out == "" {
+		println!("No outputfile specificed");
+		return
+	}
+
+	let ref input_filename = args.flag_in;
+	let file = match File::open(input_filename) {
+		Ok(file) => file,
+		Err(e) => {
+			println!("Failed to open file: '{}' Error: '{}'", input_filename, e);
+			return
+		}
+	};
+
+	let read_fn:fn (&mut Read) -> Result<ptmf::PTModule, ptmf::PTMFError> = ptmf::read_mod;
+
+	let mut reader = BufReader::new(&file);
+	let module = match read_fn(&mut reader) {
+		Ok(module) => module,
+		Err(e) => {
+			println!("Failed to parse file: '{}' Error: '{:?}'", input_filename, e);
+			return;
+		}
+	};
+	
+	println!("Done!");
+}
